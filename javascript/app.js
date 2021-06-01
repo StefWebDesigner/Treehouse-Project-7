1e2b47d710b38ad12ea0e2fce85e6ec05cd86b0f
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2021-06-01 18:21:42 -0400

    Gradually working on the optimizing of 768 & 1024 viewport > major progress but still have stuck grid elements that won't change.

--- javascript/app.js
+++ javascript/app.js
@@ -7,6 +7,29 @@
 // I could I use the changing property setting to save settings?????
 // REDOFOME THE DATABASE AND ADD CLASSES TO IT
 
+
+//ALERT FUNCTIONALITY
+
+const alertBanner = document.getElementById("alert");
+// create the html for the banner
+alertBanner.innerHTML = 
+`
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`
+
+alertBanner.addEventListener('click', e => {
+	const element = e.target;
+	if (element.classList.contains("alert-banner-close")) {
+	alert.style.display = "none"
+	}
+		
+	});
+
+
 //DASHBOARD fUNCTION
 const main_header = document.querySelector('div.main_header');
 const search_it = document.querySelector('input#search_it');
@@ -27,6 +50,10 @@ const dawn = new Total_Members("Dawn Wood", "dawn.wood16@example.com", "10/15/15
 const dan = new Total_Members("Dan Oliver", "dan.oliver82@example.com", "10/15/15");
 
 //DASHBOARD LISTENER
+/* <a class="profile-email" href="#">victoria.chambers80@example.com</a> */
+//Could the href help?????
+
+
 search_it.addEventListener('keyup', (event) => {
 //TEST
 	console.log('main header listerer working...yea haww');
@@ -67,21 +94,42 @@ traffic_nav.addEventListener('click', (event) => {
 
 //WHEN I CLICK INBETTEEN EACH MENU OPTION, IT COMPLETLEY CHANGES TO SOMETHING ELSE
 //iF i CLICK THE ITEM, IT MAKE A DOT INSTEAD OF APPLY THE STYLING
-	if (traffic_nav_indicator.className = "traffic-nav-link-1") {
+// for (let i; traffic_nav.length; i++) {
+	if (traffic_nav_indicator.className = "traffic-nav-hourly") {
 		console.log('The hourly thing is responding');
-		traffic_nav_indicator.className += 'selected-traffic-nav';
-	} if (traffic_nav_indicator.className = "traffic-nav-link-2") {
+		traffic_nav_indicator.className = 'selected-traffic-nav';
+	} if (traffic_nav_indicator.className = "traffic-nav-daily") {
 		console.log('The daily thing is responding');
-		traffic_nav_indicator.className += 'selected-traffic-nav';
-	} if (traffic_nav_indicator.className = "traffic-nav-link-active-3") {
+		traffic_nav_indicator.className = 'selected-traffic-nav';
+	} if (traffic_nav_indicator.className = "traffic-nav-weekly") {
 		console.log('The weekly thing is responding');
-		traffic_nav_indicator.className += 'selected-traffic-nav';
-	} if (traffic_nav_indicator.className = "traffic-nav-link-4") {
+		traffic_nav_indicator.className = 'selected-traffic-nav';
+	} if (traffic_nav_indicator.className = "traffic-nav-monthly") {
 		console.log('The montlh thing is responding');
-		traffic_nav_indicator.className += 'selected-traffic-nav';
+		traffic_nav_indicator.className = 'selected-traffic-nav';
 	}
 
+// }
+
 });
+
+// if (traffic_nav_indicator.className = "traffic-nav-link-1") {
+// 	console.log('The hourly thing is responding');
+// 	traffic_nav_indicator.className = 'selected-traffic-nav';
+// } if (traffic_nav_indicator.className = "traffic-nav-link-2") {
+// 	console.log('The daily thing is responding');
+// 	traffic_nav_indicator.className = 'selected-traffic-nav';
+// } if (traffic_nav_indicator.className = "traffic-nav-link-active-3") {
+// 	console.log('The weekly thing is responding');
+// 	traffic_nav_indicator.className = 'selected-traffic-nav';
+// } if (traffic_nav_indicator.className = "traffic-nav-link-4") {
+// 	console.log('The montlh thing is responding');
+// 	traffic_nav_indicator.className = 'selected-traffic-nav';
+// }
+
+// });
+
+
 
 //EMAIL NOTIFICATION SETTINGS FUNCTION
 const email_settings = document.querySelector('label#email_group');
@@ -122,36 +170,56 @@ profile_settings.addEventListener('click', (event) => {
 		// }
 });
 
+// MESSAGING FUNCTIONALITY
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+	// ensure user and message fields are filled out
+	if (user.value === "" && message.value === "") {
+	alert("Please fill out user and message fields before sending");
+	} else if (user.value === "" ) {
+	alert("Please fill out user field before sending");
+	} else if (message.value === "" ) {
+	alert("Please fill out message field before sending");
+	} else {
+	alert(`Message successfully sent to: ${user.value}`);
+	}
+});
+
+
 // SAVE SETTINGS FUNCTION
 // const settings = document.querySelector('fieldset.switch-toggle');
 const settings = document.querySelector('select#timezone');
+// const optional_responses = document.querySelector('option#timezone_options');
 const optional_responses = document.querySelector('option#timezone_options');
 
 //Listening Event
 settings.addEventListener('click', (event) => {
-		console.log('Im in the function');
+		console.log('Im in the time function function');
 //LISTEN FOR INPUT
 		selected_input_time = event.target;
 		// let input_memory = [];
 		// input_memory.push(selected_input_time);
 		if (selected_input_time === optional_responses) {
 			console.log("I'm in the timezone if statement");
-			 if (optional_responses.className = "est-zone") {
+			 if (optional_responses.value = "est-zone") {
 				console.log('green light on Eastern');
 				alert('Time zone has been updated to Eastern zone');
-			} else if (optional_responses.className = "pac-zone") {
+			} else if (optional_responses.value = "pac-zone") {
 				console.log('green light on Pacific');
 				alert('Time zone has been updated to Pacific zone');
-			} else if (optional_responses.className = "gm-zone") {
+			} else if (optional_responses.value = "gm-zone") {
 				console.log('green light on GM');
 				alert('Time zone has been updated to GM Central zone');
-			} else if (optional_responses.className = "java-zone") {
+			} else if (optional_responses.value = "java-zone") {
 				console.log('green light on java');
 				alert('Time zone has been updated to Javascript Time zone zone');
-			} else if (optional_responses.className = "code-zone") {
+			} else if (optional_responses.value = "code-zone") {
 				console.log('green light on code');
 				alert('Time zone has been updated to Coding Time zone zone');
-			} else if (optional_responses.className = "jedi-zone") {
+			} else if (optional_responses.value = "jedi-zone") {
 				console.log('green light on Jedi');
 				alert('Young padawan, time is only the force within you - Jedi Force Time zone zone');
 			} else {
@@ -161,6 +229,35 @@ settings.addEventListener('click', (event) => {
 		}
 
 	});
+
+
+
+
+	// if (selected_input_time === optional_responses) {
+	// 	console.log("I'm in the timezone if statement");
+	// 	 if (optional_responses.className = "est-zone") {
+	// 		console.log('green light on Eastern');
+	// 		alert('Time zone has been updated to Eastern zone');
+	// 	} else if (optional_responses.className = "pac-zone") {
+	// 		console.log('green light on Pacific');
+	// 		alert('Time zone has been updated to Pacific zone');
+	// 	} else if (optional_responses.className = "gm-zone") {
+	// 		console.log('green light on GM');
+	// 		alert('Time zone has been updated to GM Central zone');
+	// 	} else if (optional_responses.className = "java-zone") {
+	// 		console.log('green light on java');
+	// 		alert('Time zone has been updated to Javascript Time zone zone');
+	// 	} else if (optional_responses.className = "code-zone") {
+	// 		console.log('green light on code');
+	// 		alert('Time zone has been updated to Coding Time zone zone');
+	// 	} else if (optional_responses.className = "jedi-zone") {
+	// 		console.log('green light on Jedi');
+	// 		alert('Young padawan, time is only the force within you - Jedi Force Time zone zone');
+	// 	} else {
+	// 		alert('Unable the update time zone');
+	// 	}
+
+	// }
 //=======================================
 //         ARCHIVES WORKING
 //=======================================
