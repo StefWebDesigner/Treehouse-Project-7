e79efc1295ad6a45073fdc3439b101d4cdb55f5d
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2021-07-21 14:21:52 -0400

    +> update function active -> its not display & chartData is not a function problem

--- javascript/app.js
+++ javascript/app.js
@@ -33,23 +33,15 @@ alertBanner.addEventListener('click', e => {
 //TRAFFIC NAVIGATION FUNCTIONALITY
 
 
-// Global traffic values
+// Global traffic values *** this has a issue if included anywhere
 const traffic_nav_hourly = document.querySelector('#traffic-nav-hourly');
 const traffic_nav_daily = document.querySelector('#traffic-nav-daily');
 const traffic_nav_weekly = document.querySelector('#traffic-nav-weekly');
 const traffic_nav_monthly = document.querySelector('#traffic-nav-monthly');
 
+// function navHighlight(trafficData) {
 
-//*** MAYBE NEED A PARAMETER...BUT WTF WHAT!!!! */
-function navHighlight() {
-//-------------------- chart variables not working ------------------
-//--------------------- where do I enter this bs---------------------
-// //   var myChart = new Chart(
-//     document.getElementById('myChart'),
-//     config
-//   );
-
-//CHART 1 (HOURLY)
+//CHART 1 (HOURLY) *** This has an issue if not connected with the update function
 const hourlyData = {
 			type: 'line',  
 			data: {
@@ -77,33 +69,7 @@ const hourlyData = {
 
 	};
 
-// const hourlyData = new Chart(chart1, {
-// 			type: 'line',  
-// 			data: {
-// 			labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
-// 			datasets: [{
-// 			label: "Traffic",
-// 			type: "line",
-// 		fillColor: "#826AEE",
-// 			borderColor: "#8e5ea2",
-// 			data: [550,1250,1000,2000, 1500, 1750, 1250, 1750, 2250, 1500, 2500],
-// 			backgroundColor: "#826AEE",
-// 			backgroundColorHover: "#3e95cd",
-// 			fill: false,
-// 			}]
-// 			},
-// 			options: {
-// 			legend: { display: false },
-// 			elements: {
-//         	line: {
-//             tension: .45
-// 		}
-// 			},											
-// 		}
-// 	});
-
 //CHART 2(DAILY)
-//VARIABLES FOR EACH CHART >>> TO BE CALLED ON
 const dailyData = {
 		type: 'line',  
 		data: {
@@ -185,44 +151,43 @@ const monthlyData = {
 		}
 	};
 
-}
+
+// Just reference to the root DOM points
+// const traffic_nav_hourly = document.querySelector('#traffic-nav-hourly');
+// const traffic_nav_daily = document.querySelector('#traffic-nav-daily');
+// const traffic_nav_weekly = document.querySelector('#traffic-nav-weekly');
+// const traffic_nav_monthly = document.querySelector('#traffic-nav-monthly');
 
 //FUNCTION TO HAVE THE CHART DATA READY FOR THE FINAL TRAFFIC CALL
+//*** I need to add some referenceh to somethign */
 	function update(chartData) {
 			console.log('update function working');
-
-			//---------declaring it still DOESN'T WORK!!!! -----------------------------------
-		//============ added the full chart tag indicated by the website...no f*** luck!
-				//============ rendering doesn't work!!! 
 		
-			
 			if (chartData.contains(hourlyData)) {
-				hourlyData = new Chart(document.querySelector('canvas#chart1', hourlyData)).inner;
-				update(hourlyData).document.querySelector('canvas#chart1').innerHTML;
-				hourlyData.render();
+				hourlyData = new Chart(document.querySelector('traffic_nav_hourly', hourlyData)).innerHTML;
+				// hourlyData = new Chart(document.querySelector('canvas#chart1', hourlyData)).innerHTML;
 				console.log('update function: hourlyData processed');
+				return update(hourlyData).document.querySelector('canvas#chart1').innerHTML;
 			} if (chartData.contains(dailyData)) {
-				dailyData = new Chart(document.querySelector('canvas#chart1', dailyData)).inner;
-				update(dailyData).document.querySelector('canvas#chart1').innerHTML;
-				dailyData.render();
+				dailyData = new Chart(document.querySelector('canvas#chart1', dailyData)).innerHTML;
 				console.log('update function: dailyData processed');
+				return update(dailyData).document.querySelector('canvas#chart1').innerHTML;
 			} if (chartData.contains(weeklyData)) {
-				weeklyData = new Chart(document.querySelector('canvas#chart1', weeklyData)).inner;
-				update(weeklyData).document.querySelector('canvas#chart1').innerHTML;
-				weeklyData.render();
+				weeklyData = new Chart(document.querySelector('canvas#chart1', weeklyData)).innerHTML;
 				console.log('update function: weeklyData processed');
+				return update(weeklyData).document.querySelector('canvas#chart1').innerHTML;
 			} if (chartData.contains(monthlyData)) {
-				monthlyData = new Chart(document.querySelector('canvas#chart1', monthlyData)).inner;
-				update(monthlyData).document.querySelector('canvas#chart1').innerHTML;
-				monthlyData.render();
+				monthlyData = new Chart(document.querySelector('canvas#chart1', monthlyData)).innerHTML;
 				console.log('update function: monthlyData processed');
-		}
+				return update(monthlyData).document.querySelector('canvas#chart1').innerHTML;
+			}
+//temporarly delted: end update tags			
 }
 
+// It needs several things::: 1) to find the stupd root point, 2) which update chart, 3) which to highligh
+//maybe it requires two perameters
+
 traffic.addEventListener('click', (e) => {
-	//+++++++++++++++the traffic nav if condition is working +++++++++++++++
-	//+++++++++++++++ the highlighting is fucking working too++++++++++
-	//test
 	console.log('traffic listening');
 
 	//----------------- find out how to break the add event------------
@@ -230,40 +195,123 @@ traffic.addEventListener('click', (e) => {
 	if (e.target.classList.contains('hourly')) {
 			console.log('if condition working?');
 		  e.target.classList.add('active');
-		  //CALLING THE SPECIFIC DATA CHART
-		  update(hourlyData);
-		  //CALLING THE FUNCTION
-		  navHighlight(traffic-nav-hourly);
 		  console.log('Traffic nav: hourlyData active now');
-
+		  return update(hourlyData);
 		}
 		if (e.target.classList.contains('daily')) {
 		  e.target.classList.add('active');
-		  update(dailyData);
-		  navHighlight(traffic-nav-daily);
 		  console.log('Traffic nav: dailyData active now');
-
+		  return update(dailyData);
 		}
 		if (e.target.classList.contains('weekly')) {
 		   e.target.classList.add('active');
-		   update(weeklyData);
-		   navHighlight(traffic-nav-weekly);
 		   console.log('Traffic nav: weeklyData active now');
+		   return update(weeklyData);
 
 		}
 		if (e.target.classList.contains('monthly')) {
 		   e.target.classList.add('active');
-		   update(monthlyData);
-		   navHighlight(traffic-nav-monthly);
 		   console.log('Traffic nav: monthlyData active now');
+		   return update(monthlyData);
+
 		} 
 		// false {
 			//I want to reload it....how though!!!!!!!!!!!!
 		// 	// break();
 
-		// }
+		
 	  
 	});
+
+
+
+// This the experimental part
+
+
+//FUNCTION TO HAVE THE CHART DATA READY FOR THE FINAL TRAFFIC CALL
+// function update(chartData) {
+// 	console.log('update function working');
+
+// 	if (chartData.contains(hourlyData)) {
+// 		hourlyData = new Chart(document.querySelector('canvas#chart1', hourlyData)).innerHTML;
+// 		hourlyData.render();
+// 		console.log('update function: hourlyData processed');
+// 		return update(hourlyData).document.querySelector('canvas#chart1').innerHTML;
+// 	} if (chartData.contains(dailyData)) {
+// 		dailyData = new Chart(document.querySelector('canvas#chart1', dailyData)).innerHTML;
+// 		dailyData.render();
+// 		console.log('update function: dailyData processed');
+// 		return update(dailyData).document.querySelector('canvas#chart1').innerHTML;
+// 	} if (chartData.contains(weeklyData)) {
+// 		weeklyData = new Chart(document.querySelector('canvas#chart1', weeklyData)).innerHTML;
+// 		weeklyData.render();
+// 		console.log('update function: weeklyData processed');
+// 		return update(weeklyData).document.querySelector('canvas#chart1').innerHTML;
+// 	} if (chartData.contains(monthlyData)) {
+// 		monthlyData = new Chart(document.querySelector('canvas#chart1', monthlyData)).innerHTML;
+// 		monthlyData.render();
+// 		console.log('update function: monthlyData processed');
+// 		return update(monthlyData).document.querySelector('canvas#chart1').innerHTML;
+// 	}
+//temporarly delted: end update tags			
+// }
+
+
+// traffic.addEventListener('click', (e) => {
+
+// function navHighlight(trafficData) {
+
+
+//+++++++++++++++the traffic nav if condition is working +++++++++++++++
+//+++++++++++++++ the highlighting is fucking working too++++++++++
+//test
+// console.log('traffic listening');
+
+//----------------- find out how to break the add event------------
+
+// if (e.target.classList.contains('hourly')) {
+// 	console.log('if condition working?');
+//   e.target.classList.add('active');
+//   //CALLING THE SPECIFIC DATA CHART
+//   update(hourlyData);
+//   //CALLING THE FUNCTION
+//   navHighlight(traffic-nav-hourly);
+//   console.log('Traffic nav: hourlyData active now');
+
+// }
+// if (e.target.classList.contains('daily')) {
+//   e.target.classList.add('active');
+//   update(dailyData);
+//   navHighlight(traffic-nav-daily);
+//   console.log('Traffic nav: dailyData active now');
+
+// }
+// if (e.target.classList.contains('weekly')) {
+//    e.target.classList.add('active');
+//    update(weeklyData);
+//    navHighlight(traffic-nav-weekly);
+//    console.log('Traffic nav: weeklyData active now');
+
+// }
+// if (e.target.classList.contains('monthly')) {
+//    e.target.classList.add('active');
+//    update(monthlyData);
+//    navHighlight(traffic-nav-monthly);
+//    console.log('Traffic nav: monthlyData active now');
+// } 
+// // false {
+// 	//I want to reload it....how though!!!!!!!!!!!!
+// // 	// break();
+
+// }
+
+// });
+
+
+
+
+
+
 // // ===================
 //************************HOLD OFF ON THIS**********************************
 
